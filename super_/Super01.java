21cd6018b1bab166b6885c4de65951f1d523fa2e
银小海 <643013242@QQ.COM>
2023-10-18 11:13:11 +0800

    Delete super_ directory

--- super_/Super01.java
+++
@@ -1,8 +0,0 @@
-package com.yinhai.super_;
-
-public class Super01 {
-    public static void main(String[] args) {
-
-    }
-}
-
--- super_/superDetail.java
+++
@@ -1,5 +0,0 @@
-package com.yinhai.super_;
-
-public class superDetail {
-
-}
