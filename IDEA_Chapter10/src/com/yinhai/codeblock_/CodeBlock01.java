8ca77ffa03c0ace99e2c34c0db4448748afac46f
银小海 <643013242@QQ.COM>
2023-10-23 15:16:16 +0800

    代码块的引出和快速入门

---
+++ IDEA_Chapter10/src/com/yinhai/codeblock_/CodeBlock01.java
@@ -0,0 +1,9 @@
+package com.yinhai.codeblock_;
+
+public class CodeBlock01 {
+    public static void main(String[] args) {
+        Moive movie = new Moive("xiaowang");
+        Moive movie1 = new Moive("xiaochen",100);
+        Moive movie2 = new Moive("xiaohuang",100,"1000");
+    }
+}
