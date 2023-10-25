481b8d8d9d0d429021f8ad2fab4f46cf031e68f6
银小海 <643013242@QQ.COM>
2023-10-25 21:35:23 +0800

    @Override
     @Override表示指定重写父类的方法(从编译层面验证)

---
+++ IDEA_Chapter11/src/com/yinhai/annotation_/Override01.java
@@ -0,0 +1,46 @@
+package com.yinhai.annotation_;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+public class Override01 {
+    public static void main(String[] args) {
+        Son son = new Son();
+
+    }
+}
+class Father{//父类
+    public void fly(){
+        int i = 0;
+        System.out.println("Father fly...");
+    }
+    public void say(){
+    }
+}
+class Son extends Father {//子类
+    //1. @Override 注解放在fly方法上，表示子类的fly方法时重写了父类的fly
+    //2. 这里如果没有写 @Override 还是重写了父类fly
+    //3. 如果你写了@Override注解，编译器就会去检查该方法是否真的重写了父类的
+    //   方法，如果的确重写了，则编译通过，如果没有构成重写，则编译错误
+    //4. 看看 @Override的定义
+    //   解读： 如果发现 @interface 表示一个 注解类
+    /*
+        @Target(ElementType.METHOD)
+        @Retention(RetentionPolicy.SOURCE)
+        public @interface Override {
+        }
+     */
+     //说明
+
+    @Override
+    public void fly() {
+        super.fly();
+    }
+
+    @Override
+    public void say() {
+
+    }
+}
+
