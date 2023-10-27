2bf7e54607d6e2d122aec508984d933a595c041a
银小海 <643013242@QQ.COM>
2023-10-27 23:00:45 +0800

    八大Wrapper类
    Double,Short,Float,Inteter,Byte五类为基本数据类型继承Number类的包装类，Boolean,Character包装类

---
+++ IDEA_Chapter13/src/com/yinhai/wrapper_/WrapperType.java
@@ -0,0 +1,19 @@
+package com.yinhai.wrapper_;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+public class WrapperType {
+    public static void main(String[] args) {
+
+        //boolean -> Boolean
+        //char -> Character
+        //byte -> Byte
+        //long -> Long
+        //float -> Float
+        //double -> Double
+        //short -> Short
+
+    }
+}
