b4e596d9078c5072034cd919d59bb92baf550517
银小海 <643013242@QQ.COM>
2023-10-28 19:38:06 +0800

    StringBuilder的基本介绍
    单线程可用，效率高，理解为buffer的补充

---
+++ IDEA_Chapter13/src/com/yinhai/wrapper_/stringbuilder_/StringBuilder01.java
@@ -0,0 +1,17 @@
+package com.yinhai.stringbuilder_;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+
+public class StringBuilder01 {
+    public static void main(String[] args) {
+        //1. StringBuilder 继承 AbstractStringBuilder 类
+        //2. 实现了 Serializable ,说明StringBuilder对象是可以串行化(对象可以网络传输,可以保存到文件)
+        //3. StringBuilder 是final类, 不能被继承
+        //4. StringBuilder 对象字符序列仍然是存放在其父类 AbstractStringBuilder的 char[] value  因此，字符序列是堆中
+        //5. StringBuilder 的方法，没有做互斥的处理,即没有synchronized 关键字,因此在单线程的情况下使用StringBuilder
+        StringBuilder stringBuilder = new StringBuilder();
+    }
+}
