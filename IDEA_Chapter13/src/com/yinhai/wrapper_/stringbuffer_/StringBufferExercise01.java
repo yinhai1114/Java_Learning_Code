cc3472fb3e767c1ae9a03c2c5a953ce76a744c28
银小海 <643013242@QQ.COM>
2023-10-28 19:36:45 +0800

    StringBuffer的两个练习
    都蛮重要的

---
+++ IDEA_Chapter13/src/com/yinhai/wrapper_/stringbuffer_/StringBufferExercise01.java
@@ -0,0 +1,21 @@
+package com.yinhai.stringbuffer_;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+
+public class StringBufferExercise01 {
+    public static void main(String[] args) {
+        String str = null;// ok
+        StringBuffer sb = new StringBuffer(); //ok
+        sb.append(str);//需要看源码 , 底层调用的是 AbstractStringBuilder 的 appendNull () 增加了null字符
+        System.out.println(sb.length());//4
+
+        System.out.println(sb);//null
+        //下面的构造器，会抛出NullpointerException
+        StringBuffer sb1 = new StringBuffer(str);//看底层源码 super(str.length() + 16);
+        System.out.println(sb1);
+
+    }
+}
