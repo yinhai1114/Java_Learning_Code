d15f00d60a74b0759ab0969c560dcf6df37786c0
银小海 <643013242@QQ.COM>
2023-10-29 22:09:05 +0800

    Collectionl类的入门查看结构图

--- IDEA_Chapter14/src/com/yinhai/collection_/Collection_.java
+++ IDEA_Chapter14/src/com/yinhai/collection_/Collection_.java
@@ -1,1 +1,15 @@
+package com.yinhai.collection_;
 
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+public class Collection_ {
+    public static void main(String[] args) {
+        // Collection
+        // Map
+    }
+}
