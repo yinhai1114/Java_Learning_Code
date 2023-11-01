fc8cc797c20c2a2a51c5584c79185eada40d79a0
银小海 <643013242@QQ.COM>
2023-11-01 22:52:28 +0800

    Map的源码

---
+++ IDEA_Chapter14/src/com/yinhai/map_/MapSource_.java
@@ -0,0 +1,52 @@
+package com.yinhai.map_;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+
+public class MapSource_ {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("no1", "韩顺平");//k-v
+        map.put("no2", "张无忌");//k-v
+        map.put(new Car(), new Person());//k-v
+        System.out.println(map.getClass());
+        //1. k-v 最后是 HashMap$Node node = newNode(hash, key, value, null)
+        //2. k-v 为了方便程序员的遍历，还会 创建 EntrySet 集合 ，该集合存放的元素的类型 Entry, 而一个Entry
+        //即把Node转成Entry然后再放到EntrySet里 (引用),指向而不是创建!
+        //   对象就有k,v EntrySet<Entry<K,V>> 即： transient Set<Map.Entry<K,V>> entrySet;
+        //3. 在entrySet中，定义的类型是 Map.Entry ，但是实际上存放的还是 HashMap$Node Node实现了Entry的接口
+        //   这时因为 static class Node<K,V> implements Map.Entry<K,V>
+        Set set = map.entrySet();
+        System.out.println(set.getClass());// HashMap$EntrySet
+        for (Object obj : set) {
+
+            //System.out.println(obj.getClass()); //HashMap$Node
+            //为了从 HashMap$Node 取出k-v
+            //1. 先做一个向下转型
+            Map.Entry entry = (Map.Entry) obj;
+            System.out.println(entry.getKey() + "-" + entry.getValue() );
+            //4. 当把 HashMap$Node 对象 存放到 entrySet 就方便我们的遍历, 因为 Map.Entry 提供了重要方法
+            //   K getKey(); V getValue();
+        }
+        //可以把key值封装为KeySet，values封装成Values
+        Set set1 = map.keySet();
+        System.out.println(set1.getClass());
+        Collection values = map.values();
+        System.out.println(values.getClass());
+    }
+}
+
+class Car {
+
+}
+
+class Person{
+
+}
