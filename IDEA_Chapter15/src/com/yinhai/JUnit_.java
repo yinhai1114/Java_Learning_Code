f686f2e4551914764937fb375cf64bea437ebb91
银小海 <643013242@QQ.COM>
2023-11-02 22:37:59 +0800

    JUnit测试框架的使用

---
+++ IDEA_Chapter15/src/com/yinhai/JUnit_.java
@@ -0,0 +1,33 @@
+package com.yinhai;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+public class JUnit_ {
+    public static void main(String[] args) {
+        //传统方式 newJunit_（）
+    }
+
+    @Test
+    public void m1() {
+        System.out.println("m1方法被调用");
+    }
+
+    @Test
+    public void m2() {
+        System.out.println("m2方法被调用");
+    }
+
+    @Test
+    public void m3() {
+        System.out.println("m3方法被调用");
+    }
+}
+
+
+
+
+
