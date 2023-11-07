10feb24f1a601367e648467f8f22c3de5acb2980
银小海 <643013242@QQ.COM>
2023-11-07 23:35:27 +0800

    字节节点流

--- IDEA_Chapter19/src/com/yinhai/inputstream_/FileInputStream_.java
+++ IDEA_Chapter19/src/com/yinhai/inputstream_/FileInputStream_.java
@@ -1,1 +1,62 @@
+package com.yinhai.inputstream_;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * @author 银海
+ * @version 1.0
+ * 单个字节的读取
+ */
+public class FileInputStream_ {
+    public static void main(String[] args) {
+
+    }
+    @Test
+    public void readFile01() {
+        String filePath = "e:\\hello.txt";
+        FileInputStream fileInputStream = null;
+        int readDate = 0;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+            //如何返回-1，表示读取完毕
+            while ((readDate = fileInputStream.read()) != -1) {
+                System.out.print((char) readDate);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileInputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+@Test
+    public void readFile02() {
+        String filePath = "e:\\hello.txt";
+        FileInputStream fileInputStream = null;
+        int readDate = 0;
+        byte[] buf = new byte[8];
+        int readLen = 0;
+        try {
+            fileInputStream = new FileInputStream(filePath);
+            //如何返回-1，表示读取完毕
+            while ((readLen = fileInputStream.read(buf)) != -1) {
+                System.out.print(new String(buf, 0, readLen));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fileInputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
