e85bc5e71eda25ab7a79036402b8ebbb46629eda
银小海 <643013242@QQ.COM>
2023-11-07 23:36:36 +0800

    引出转换流

--- IDEA_Chapter19/src/com/yinhai/transformation/CodeQuestion.java
+++ IDEA_Chapter19/src/com/yinhai/transformation/CodeQuestion.java
@@ -1,1 +1,23 @@
+package com.yinhai.transformation;
 
+import java.io.*;
+
+/**
+ * @author 银海
+ * @version 1.0
+ * 看一个中文乱码问题
+ */
+public class CodeQuestion {
+    public static void main(String[] args) throws IOException {
+        //读取e:\\a.txt 文件到程序
+        //思路
+        //1.  创建字符输入流 BufferedReader [处理流]
+        //2. 使用 BufferedReader 对象读取a.txt
+        //3. 默认情况下，读取文件是按照 utf-8 编码，但可能不一定是utf-8编码，就会出现乱码情况
+        String filePath = "e:\\a.txt";
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        String s = br.readLine();
+        System.out.println("读取到的内容: " + s);
+        br.close();
+    }
+}
