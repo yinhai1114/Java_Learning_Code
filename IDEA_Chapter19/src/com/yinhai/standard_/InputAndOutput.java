40ce469040616cdee34cced3fccd102ce15f02cb
银小海 <643013242@QQ.COM>
2023-11-07 23:36:03 +0800

    标准输入输出流

---
+++ IDEA_Chapter19/src/com/yinhai/standard_/InputAndOutput.java
@@ -0,0 +1,32 @@
+package com.yinhai.standard_;
+
+import java.util.Scanner;
+
+/**
+ * @author 银海
+ * @version 1.0
+ */
+public class InputAndOutput {
+    public static void main(String[] args) {
+        //System 类 的 public final static InputStream in = null;
+        // System.in 编译类型   InputStream
+        // System.in 运行类型   BufferedInputStream
+        // 表示的是标准输入 键盘
+        System.out.println(System.in.getClass());
+
+        //1. System.out public final static PrintStream out = null;
+        //2. 编译类型 PrintStream
+        //3. 运行类型 PrintStream
+        //4. 表示标准输出 显示器
+        System.out.println(System.out.getClass());
+
+        //传统方法System.out.println(); 是使用out对象将数据输出到显示器
+        System.out.println("hello, 韩顺平教育~");
+
+        //传统的方法，Scanner是从标准输入键盘接收数据
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("输入内容");
+        String next = scanner.next();
+        System.out.println("next=" + next);
+    }
+}
