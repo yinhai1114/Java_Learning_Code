f96d40b37853c47b1455f3997f3196ff6ec86214
银小海 <643013242@QQ.COM>
2023-11-14 16:13:12 +0800

    类加载的连接阶段的演示

---
+++ IDEA_Chapter23/src/com/yinhai/reflection/ClassLoad02_.java
@@ -0,0 +1,23 @@
+package com.yinhai.reflection.classload_;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class ClassLoad02_ {
+    public static void main(String[] args) {
+
+    }
+}
+class A {
+    //属性-成员变量-字段
+    //链接阶段-准备 属性是如何处理
+    //1. n1 是实例属性, 不是静态变量，因此在准备阶段，是不会分配内存
+    //2. n2 是静态变量，分配内存 n2 是默认初始化 0 ,而不是20
+    //3. n3 是static final 是常量, 他和静态变量不一样, 因为一旦赋值就不变 n3 = 30
+    public int n1 = 10;
+    public static  int n2 = 20;
+    public static final  int n3 = 30;
+}
+
