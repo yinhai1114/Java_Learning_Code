f47d675a576fee66d514724218740b1a5bca5a34
银小海 <643013242@QQ.COM>
2023-11-14 20:08:47 +0800

    通过反射访问操作访问属性

---
+++ IDEA_Chapter23/src/com/yinhai/reflection/ReflecAccessProperty.java
@@ -0,0 +1,48 @@
+package com.yinhai.reflection;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ * 演示操作属性
+ */
+public class ReflecAccessProperty {
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException, NoSuchFieldException {
+
+        //1. 得到Student类对应的 Class对象
+        Class<?> stuClass = Class.forName("com.yinhai.reflection.Student");
+        //2. 创建对象
+        Object o = stuClass.newInstance();//o 的运行类型就是Student
+        System.out.println(o.getClass());//Student
+        //3. 使用反射得到age 属性对象
+        Field age = stuClass.getField("age");
+        age.set(o, 88);//通过反射来操作属性
+        System.out.println(o);//
+        System.out.println(age.get(o));//返回age属性的值
+
+        //4. 使用反射操作name 属性
+        Field name = stuClass.getDeclaredField("name");
+        //对name 进行暴破, 可以操作private 属性
+        name.setAccessible(true);
+        //name.set(o, "老韩");//也能使用对象操作
+        name.set(null, "老韩~");//因为name是static属性，因此 o 也可以写出null
+        System.out.println(o);
+        System.out.println(name.get(o)); //获取属性值
+        System.out.println(name.get(null));//获取属性值, 要求name是static
+
+    }
+}
+
+class Student {//类
+    public int age;
+    private static String name;
+
+    public Student() {//构造器
+    }
+
+    public String toString() {
+        return "Student [age=" + age + ", name=" + name + "]";
+    }
+}
