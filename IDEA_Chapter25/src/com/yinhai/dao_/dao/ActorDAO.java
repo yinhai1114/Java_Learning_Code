0e78f25020bf045c5bb893038fa122692f72f4f2
银小海 <643013242@QQ.COM>
2023-11-26 23:20:12 +0800

    DAO和增删改查通用方法-BasicDao的存放XxxDAO和BasicDAO

---
+++ IDEA_Chapter25/src/com/yinhai/dao_/dao/ActorDAO.java
@@ -0,0 +1,14 @@
+package com.yinhai.dao_.dao;
+
+
+import com.yinhai.dao_.domain.Actor;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class ActorDAO extends BasicDAO<Actor>{
+    //1.就有BasicDAO的方法
+    //2.根据业务需求可以编写特有的方法
+}
