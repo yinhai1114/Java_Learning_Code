83b2c1daa5b9dee598173d6126cbc3d889c25fb4
银小海 <643013242@QQ.COM>
2023-11-27 23:46:45 +0800

    DAO类 对应直接修改数据库的Bill表

---
+++ IDEA_Chapter26/src/com/yinhai/mhl/dao/BillDAO.java
@@ -0,0 +1,11 @@
+package com.yinhai.mhl.dao;
+
+import com.yinhai.mhl.domain.Bill;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class BillDAO extends BasicDAO<Bill> {
+}
