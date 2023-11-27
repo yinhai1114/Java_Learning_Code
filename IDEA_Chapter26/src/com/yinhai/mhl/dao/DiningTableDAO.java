05799b16b4eb939dbd7f842f1d35da8f5dfbfa87
银小海 <643013242@QQ.COM>
2023-11-27 23:46:53 +0800

    DAO类 对应直接修改数据库的餐桌表

---
+++ IDEA_Chapter26/src/com/yinhai/mhl/dao/DiningTableDAO.java
@@ -0,0 +1,12 @@
+package com.yinhai.mhl.dao;
+
+import com.yinhai.mhl.domain.DiningTable;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class DiningTableDAO extends BasicDAO<DiningTable> {
+
+}
