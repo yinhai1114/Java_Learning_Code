3d6e04a8b314498f94e969b996839298a8afad51
银小海 <643013242@QQ.COM>
2023-11-27 23:47:00 +0800

    DAO类 对应直接修改数据库的雇员表

---
+++ IDEA_Chapter26/src/com/yinhai/mhl/dao/EmployeeDAO.java
@@ -0,0 +1,12 @@
+package com.yinhai.mhl.dao;
+
+import com.yinhai.mhl.domain.Employee;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class EmployeeDAO extends BasicDAO<Employee>{
+
+}
