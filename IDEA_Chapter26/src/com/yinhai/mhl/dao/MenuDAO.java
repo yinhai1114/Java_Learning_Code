d128325968ea6b8aa35fad4d95ea3b3f5d9c201b
银小海 <643013242@QQ.COM>
2023-11-27 23:47:06 +0800

    DAO类 对应直接修改数据库的菜单表

---
+++ IDEA_Chapter26/src/com/yinhai/mhl/dao/MenuDAO.java
@@ -0,0 +1,11 @@
+package com.yinhai.mhl.dao;
+
+import com.yinhai.mhl.domain.Menu;
+
+/**
+ * @author 银小海
+ * @version 1.0
+ * @email yinhai14@qq.com
+ */
+public class MenuDAO extends BasicDAO<Menu> {
+}
